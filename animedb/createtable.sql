2979ffabb51a96bd5d07290a727d08ab44b54720
halfer53 <brucetansh@gmail.com>
2017-05-11 17:01:59 -0700

    add employee table

--- animedb/createtable.sql
+++ animedb/createtable.sql
@@ -61,3 +61,9 @@
 		primary key(id),
 		foreign key(customer_id) references customers(id),
 		foreign key(anime_id) references animes(id))DEFAULT CHARACTER SET=utf8;
+
+		create table employees(
+		email varchar(50) primary key,
+		password varchar(20) not null,
+		fullname varchar(100),
+		)DEFAULT CHARACTER SET=utf8;
